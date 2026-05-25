26f0019c7afdac274a11436e86ac329d0b782ee5
Shubham Bhardwaj <shubhambhardwaj11@gmail.com>
2026-05-25 22:12:27 +0530

    login error

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -24,7 +24,7 @@ async function login(){
     const email = document.getElementById('email').value
     const password = document.getElementById('password').value
     
-    const response = await fetch('http://localhost:5000/api/auth/login',{
+    const response = await fetch('https://hire-ready-i2ot.onrender.com/api/auth/login',{
         method : 'POST',
         headers : {'content-Type' : 'application/json'},
         body : JSON.stringify({email,password})
